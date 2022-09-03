cac8a657297e013096b09c29ff7a666f7a093d3b
unknown <yu.bai@jiyuncorp.com>
2022-09-04 00:08:52 +0800

    groundplane

--- groundplane/groundplane.py
+++ groundplane/groundplane.py
@@ -17,9 +17,14 @@ class GroundPlaneSimple():
         return points[mask]
 
 class GroundPlane(GroundPlaneSimple):
-    def __init__(self, pc_range):
+    def __init__(self, pc_range, max_iter=5, n_lpr=200, th_seed=0.4, th_dis=0.2):
         super().__init__(pc_range)
         self.pca = PCA(n_components=3)
+        self.n_lpr = n_lpr
+        self.th_seed = th_seed
+        self.max_iter = max_iter
+        self.th_dis = th_dis
+
 
     def compute_plane(self, points):
         '''
@@ -35,19 +40,59 @@ class GroundPlane(GroundPlaneSimple):
         norm = comp[min_index]
         d = -sum(points[0]*norm)
         return list(norm) + [d]
+    
+    def get_seeds(self, points):
+        mask0 = points[:,2]>-2.2
+        mask1 = points[:,2]<-1.4
+        points = points[np.logical_and(mask0, mask1)]
+        hs = np.sort(points[:,2])
+        averg_h = np.mean(hs[:self.n_lpr])
+        mask = points[:,2]<averg_h + self.th_seed
+        return points[mask]
+    
+    def points_2_plane_dis(self, points, plane):
+        plane = np.asarray(plane)
+        dis = np.abs(np.dot(points, plane[:3]) + plane[3])/np.linalg.norm(plane[:3])
+        return dis
+    
+    def segment_ground(self, points):
+        seeds = self.get_seeds(points)
+        for _ in range(self.max_iter):
+            plane = self.compute_plane(seeds)
+            dis = self.points_2_plane_dis(points, plane)
+            mask = dis < self.th_dis
+            seeds = points[mask]
+        return plane
 
 
 if __name__ == '__main__':
     from cluster.data.base import Data
 
-    lidar_dir = 'C:\\Users\\yu.bai\\Datasets\\Kitti\\training\\velodyne'
-    lidar_type = 'bin'
-    datas = Data(lidar_dir, lidar_type)
-    pc_range = [-20, 20, -20, 20, -5, 3.0]
+    def test_compute_plane():
+        lidar_dir = 'C:\\Users\\yu.bai\\Datasets\\Kitti\\training\\velodyne'
+        lidar_type = 'bin'
+        datas = Data(lidar_dir, lidar_type)
+        pc_range = [-20, 20, -20, 20, -5, 3.0]
 
-    gp = GroundPlane(pc_range)
-    for points in datas:
-        points = gp.filter_points(points)
-        points = np.asarray([[-10.0, 2.0,1.0], [2.0, 12.0,-1.0], [2.0, -3.0, -2.0]])
-        plane = gp.compute_plane(points)
-        datas.show_points_on_ground(points, plane)
+        gp = GroundPlane(pc_range)
+        for points in datas:
+            points = gp.filter_points(points)
+            points = np.asarray([[-10.0, 2.0,1.0], [2.0, 12.0,-1.0], [2.0, -3.0, -2.0]])
+            plane = gp.compute_plane(points)
+            datas.show_points_on_ground(points, plane)
+    
+    def test_segment_ground():
+        lidar_dir = 'C:\\Users\\yu.bai\\Datasets\\Kitti\\training\\velodyne'
+        lidar_type = 'bin'
+        datas = Data(lidar_dir, lidar_type)
+        pc_range = [-50, 50, -50, 50, -5, 3.0]
+        # pc_range = [-50, 50, -50, 50, -2.2, -1.4]
+
+        gp = GroundPlane(pc_range)
+        for points in datas:
+            points = gp.filter_points(points)
+            # datas.show(points)
+            plane = gp.segment_ground(points)
+            datas.show_ground_segmentation(points, gp.points_2_plane_dis(points, plane), th=0.2)
+    
+    test_segment_ground()
